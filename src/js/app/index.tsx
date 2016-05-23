bc188cf0fe1714047d96e055b7a2cde99c7204db
Marco Acierno <marcoacierno96@gmail.com>
2016-05-23 10:22:16 +0200

    Typescript errors with React relay

--- src/js/app/index.tsx
+++ src/js/app/index.tsx
@@ -12,7 +12,15 @@ import { Home, Error404 } from '../pages';
 
 import '../../styles/app';
 
+const Relay = require('react-relay');
+
+
 require('es6-promise').polyfill();
+
+
+Relay.injectNetworkLayer(
+  new Relay.DefaultNetworkLayer('http://127.0.0.1:8000/graphql')
+);
 
 
 ReactDOM.render(<Router history={browserHistory}>
